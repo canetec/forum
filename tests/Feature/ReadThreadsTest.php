dc8af8c9700be2e9fc18da8c52666dc1cf0ff6f4
canetec <31225821+canetec@users.noreply.github.com>
2020-03-12 22:04:12 -0300

    Add tests for thread dates

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -20,33 +20,36 @@ class ReadThreadsTest extends TestCase
         $this->thread = factory(Thread::class)->create();
     }
 
-    /**
-     * @test
-     */
-    public function a_user_can_view_all_threads()
+    public function test_a_user_can_view_all_threads()
     {
         $this->get('/threads')
             ->assertSee($this->thread->title);
     }
 
-    /**
-     * @test
-     */
-    public function a_user_can_read_a_single_thread()
+    public function test_a_user_can_read_a_single_thread()
     {
         $this->get('/thread/' . $this->thread->id)
             ->assertSee($this->thread->title);
     }
 
-    /**
-     * @test
-     */
-    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    public function test_a_user_can_read_replies_that_are_associated_with_a_thread()
     {
         $reply = factory(Reply::class)
             ->create(['thread_id' => $this->thread->id]);
 
         $this->get('/thread/' . $this->thread->id)
             ->assertSee($reply->body);
+    }
+
+    public function test_a_user_can_see_when_the_thread_was_created()
+    {
+        $this->get('/threads')
+            ->assertSee($this->thread->created_at);
+    }
+
+    public function test_a_user_can_see_who_created_the_thread()
+    {
+        $this->get('/threads')
+            ->assertSee($this->thread->owner->name);
     }
 }
