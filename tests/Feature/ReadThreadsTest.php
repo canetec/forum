70c3dd5b9f2d6fcd245cf8aa4c04d4ddb993e0c1
canetec <31225821+canetec@users.noreply.github.com>
2020-03-12 21:44:19 -0300

    Added a Reply test

---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Reply;
+use App\Thread;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    private $thread;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->thread = factory(Thread::class)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_can_view_all_threads()
+    {
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_can_read_a_single_thread()
+    {
+        $this->get('/thread/' . $this->thread->id)
+            ->assertSee($this->thread->title);
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = factory(Reply::class)
+            ->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/thread/' . $this->thread->id)
+            ->assertSee($reply->body);
+    }
+}
