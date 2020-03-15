c5ee3463c97c795895a38b274da1ca385b954552
canetec <31225821+canetec@users.noreply.github.com>
2020-03-15 00:57:23 -0300

    Add doccblocks to models

--- app/Thread.php
+++ app/Thread.php
@@ -4,6 +4,29 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Thread
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $title
+ * @property string $body
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\User $owner
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Reply[] $replies
+ * @property-read int|null $replies_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Thread extends Model
 {
     public function path()
